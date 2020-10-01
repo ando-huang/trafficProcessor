8f7be2f0abf243f4a0cdab144fb8e9278487021d
ando-huang <ahuang09182000@gmail.com>
2020-10-01 11:38:09 -0400

    completed

--- videoProcessor.py
+++ videoProcessor.py
@@ -1,27 +1,30 @@
-# create a folder to store extracted images
 import os
-
-#create a folder with a name referencing the video
-# use opencv to do the job
 import cv2
 
-
-print(cv2.__version__)  # my version is 3.1.0
-
+'''
+    readVideo CREATES a FOLDER and stores JPEGs at 1 second intervals
+    Parameters:
+        path - the filename of the video to process
+'''
 def readVideo(path):
-
     vidcap = cv2.VideoCapture(path)
     folder = (os.path.splitext(path)[0]) + "IMAGEFILES"  
     os.mkdir(folder)
     count = 0.0
+    #while True:
     while True:
         success,image = vidcap.read()
         if not success:
             break
         cv2.imwrite(os.path.join(folder,"frame{:.1f}.jpg".format(count)), image)     # save frame as JPEG file
         #count is the increment between each screencap
-        count += .5
+        count += 1
     count *=2 #there are count*2 images in the folder
     print("{} images are extacted in {}.".format(count,folder))
 
-readVideo('2020_06_15_1_27_46.mp4')
+#driver code, runs through all the files available in working directory
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+for f in files:
+    breaks = f.split('.')
+    if breaks[1] == "MP4":
+        readVideo(f)
