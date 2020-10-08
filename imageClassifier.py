9d79c0d5a4b29a1aa4b3c563a622932178cb7f75
ando-huang <ahuang09182000@gmail.com>
2020-10-08 11:00:19 -0400

    first visualization

--- imageClassifier.py
+++ imageClassifier.py
@@ -3,45 +3,91 @@ import numpy as np
 import os
 import PIL
 import tensorflow as tf
-import zipfile
-import urllib, urllister
 
-from tensorflow import keras
+from tensoflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
-#Import the training dataset from (BOSCH)
-data_url = 'https://hci.iwr.uni-heidelberg.de/system/files/private/datasets/1681105773/dataset_test_riib.zip.001'
-#god i hate underscores
-
-
-#now do something with the data_path to extract the images
+import pathlib
+dataset_url = 'https://hci.iwr.uni-heidelberg.de/system/files/private/datasets/1681105773/dataset_test_riib.zip.001'
+data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
+data_dir = pathlib.Path(data_dir)
 
 batch_size = 32
-#these image size params match the extracted images from sebastian's video
 img_height = 640
 img_width = 960
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
     data_dir,
-    validation_split = 0.2
-    subset = "training"
-    seed = 123,
-    image_size = (img_height, img_width),
-    batch_size = batch_size)
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(
     data_dir,
-    validation_split = 0.2
-    subset = "validation"
-    seed = 123,
-    image_size = (img_height, img_width),
-    batch_size = batch_size)
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
-#takes the classifications from the dataset
 class_names = train_ds.class_names
-#print(class_names)
-#we want to have ['red', 'yellow', 'green', 'off']
-#FOUR CLASSSIFICATIONS
 
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+
+num_classes = 4
+
+model = Sequential([
+  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.summary()
+
+epochs=10
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+val_acc = history.history['val_accuracy']
+
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
 
