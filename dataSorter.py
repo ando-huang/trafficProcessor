5f5cf8b0704a7634148f24595b0e08d1df160596
ando-huang <ahuang09182000@gmail.com>
2020-10-15 15:01:37 -0400

    working code for daySequence1

--- dataSorter.py
+++ dataSorter.py
@@ -1,4 +1,5 @@
 '''
+    *** TARGETS DAYSEQUENCE1 ***
     dataSorter is intended to prepare the data in archive/ for imageClassifier.py
     the problem with the given data is that it isn't sorted
     this should place images in folders with corresponding classnames that would help the model
@@ -7,8 +8,8 @@
 import os
 import shutil
 import pandas as pd
-print(pd.__version__)
 
+#read in the csv corresponding to this particular folder of data
 df = pd.read_csv(
     "archive/Annotations/Annotations/daySequence1/frameAnnotationsBULB.csv",
     sep=";",
@@ -24,21 +25,20 @@ if 'go' not in files:
     os.mkdir("go")
 if 'warning' not in files:
     os.mkdir("warning")
-    
+
 # i is index, j is series of data about the file at that index
 for i, j in df.iterrows():
     # sort each image into the right folder based on the csv
-    #print(j[0].split("/")[1])
     if j[1] == 'stop': #first case
-        imgPath = j[0].split("/")[1] #should get the actual filename within the frames folder
-        #move(source, dest)
+        imgPath = j[0].split("/")[1] #gets the filename inside frames folder
         shutil.copy(imgPath, "stop")
-        #os.remove(imgPath)
+        
     elif j[1] == 'go':
         imgPath = j[0].split("/")[1] #should get the actual filename within the frames folder
         shutil.copy(imgPath, "go")
-        #os.remove(imgPath)
+        
     else:
         imgPath = j[0].split("/")[1] #should get the actual filename within the frames folder
         shutil.copy(imgPath, "warning")
-        #os.remove(imgPath)
+
+print("Sorting complete")
