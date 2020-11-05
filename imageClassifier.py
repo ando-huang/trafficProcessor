335a64e5e26d867d98b6526878fdca735b54b9dd
ando-huang <ahuang09182000@gmail.com>
2020-11-05 01:41:13 -0500

    dropout to reduce overfitting

--- imageClassifier.py
+++ imageClassifier.py
@@ -10,16 +10,16 @@ from tensorflow.keras.models import Sequential
 #!pip install pyyaml h5py --necessary to save models in HDF5 format
 
 os.chdir("archive/daySequence1/daySequence1/")
-#sorted_data folder contains the preprocessed data from data_sorter.py
-data_dir = 'sorted_data' #is a superfolder of the three classified folders
-#use this to get the corresponding filenames and the "stop" "go" tags for each file
+data_dir = 'sorted_data' 
 
+
+# *** ACTUAL MODEL STUFF ***
 batch_size = 32
 #we might want to unify all the image sizes to the smaller size (640x960)
 
 #image params for the data we want to check
 img_height = 640 
-img_width = 960
+img_width = 960 
 
 #Fix these two training data sets, currently the function fails
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
@@ -40,8 +40,7 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
 
 #all 3 of the annotation tags from the csv
 class_names = train_ds.class_names
-print(class_names)
-#class_names = ['stop', 'go', 'warning']
+print(class_names) #['stop', 'go', 'warning']
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
@@ -49,6 +48,12 @@ train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+# Notice the pixels values are now in `[0,1]`.
+print(np.min(first_image), np.max(first_image)) 
 
 num_classes = 3
 
@@ -99,4 +104,74 @@ plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
 #at this point the results of the model are available
+data_augmentation = keras.Sequential(
+  [
+    layers.experimental.preprocessing.RandomFlip("horizontal", 
+                                                 input_shape=(img_height, 
+                                                              img_width,
+                                                              3)),
+    layers.experimental.preprocessing.RandomRotation(0.1),
+    layers.experimental.preprocessing.RandomZoom(0.1),
+  ]
+)
 
+plt.figure(figsize=(10, 10))
+for images, _ in train_ds.take(1):
+  for i in range(9):
+    augmented_images = data_augmentation(images)
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(augmented_images[0].numpy().astype("uint8"))
+    plt.axis("off")
+
+model = Sequential([
+  data_augmentation,
+  layers.experimental.preprocessing.Rescaling(1./255),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Dropout(0.2),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.summary()
+
+epochs = 15
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs = epochs
+)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+os.mkdir("traffic_model")
+model.save('traffic_model/model')
